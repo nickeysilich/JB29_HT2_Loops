03d78761553c02c21f163fd12b1d637debc9fb87
nickeysilich <45591070+nickeysilich@users.noreply.github.com>
2019-09-03 11:10:08 +0300

    Add files via upload

--- Task35.java
+++ Task35.java
@@ -6,6 +6,24 @@ import java.util.Scanner;
 public class Task35 {
 
     public static void main(String[] args) {
+    	int b;
+        int c = 0;
+        int d = 0;
+
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Введите число");
+        int a = sc.nextInt();
+
+        for (int i = 1; a > 0; i++){
+            d = d + 1;
+            b = a % 10;
+            if (b % 2 == 0){
+                c++;
+            }
+            a = a / 10;
+        }
+        System.out.println("Количество четных цифр: " + c);
+
         
     }
 }
